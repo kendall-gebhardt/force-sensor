caee7c66076a3efd4d7ecfb44fa8289b21aebfbe
kendall-gebhardt <kendallt@transcriptic.com>
2018-10-16 09:16:30 -0700

    adds parameters tested on hardware

--- read_fse.py
+++ read_fse.py
@@ -6,12 +6,9 @@
 # computer. the resulting file can then be gathered via scp.              #
 ###########################################################################
 
-
-
-# some comments about this script
 import argparse
 import datetime
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import serial
 import struct
 import sys
@@ -33,9 +30,9 @@ _activity_pin = 33
 def set_globals(filename):
     """Sets modular global variables"""
     global _filename
-    _filename = filename
+    _filename = 'logs/' + filename
     
-    print "Globals set..."
+    print ("Globals set...")
     return True
 
 def sensor_init(data_format):
@@ -52,17 +49,19 @@ def sensor_init(data_format):
     if data_format == 'force':
         data_byte = b'\x66'
     else:
+        print("not valid data format")
         return False
 
     # Initialize the sensor
     s.write(init_byte)
-    s.write(data_byte)
+    # Later it would be good to include a 'zero' button that would send this init byte
+    #s.write(data_byte)
     s.close()
 
-    print "Sensor Initialized..."
+    print ("Sensor initialized...")
     return True
 
-def logs_init(_filename): 
+def logs_init(): 
     """
     Create a csv to hold the data recorded from the force sensor
     :param _filename: defined as test-timestamp.csv
@@ -71,13 +70,13 @@ def logs_init(_filename):
     # create a file and add column headers
     # I put the /logs/ directory in .gitignore but this may cause problems 
     try:
-        with open('logs/' + _filename,"w") as file:
+        with open(_filename,"w+") as file:
             file.write("Time, Force X, Force Y, Force Z\n")
     except IOError:
-        print('Could not open file')
+        print("Could not open file")
         return False
 
-    print "Logs initialized..."
+    print ("Logs initialized...")
     return True
 
 def indicator_init():
@@ -95,10 +94,10 @@ def indicator_init():
                         _negative_z_pin, _positive_z_pin, _power_pin, _activity_pin]
         GPIO.setup(led_pin_list, GPIO.OUT)
     except Exception:
-        print('Error setting up GPIOs')
+        print("Error setting up GPIOs")
         return False
 
-    print "Indicators initialized..."
+    print ("Indicators initialized...")
     return True
 
 def get_force():
@@ -140,11 +139,11 @@ def log_force(forces):
     :return: True when done, false if the file cannot be accessed
     """
     # Log data in terminal to show progress
-    print(str(forces))
+    # print(str(forces))
     
     try:
         # write data to logfile
-        with open("logs/" + _filename, "w") as file:
+        with open(_filename, "a") as file:
             file.write(str(forces[0]) + "," + str(forces[1]) + "," 
                        + str(forces[2]) + "," + str(forces[3]) + "\n")
     except IOError:
@@ -160,34 +159,36 @@ def indicate_status(forces):
     :return: True when done
     """
     # turn on lights if force is over a threshold
-    force_threshold = 5
+    force_threshold_x = 2
+    force_threshold_y = 2
+    force_threshold_z = 5
     
     # use the boolean condition to turn pin on/off
     # this does not work for multicolor LED
-    GPIO.output(_positive_x_pin, (forces[1] > force_threshold))
-    GPIO.output(_negative_x_pin, (forces[1] < (force_threshold * -1)))
-    GPIO.output(_positive_y_pin, (forces[2] > force_threshold))
-    GPIO.output(_negative_y_pin, (forces[2] < (force_threshold * -1)))
-    GPIO.output(_positive_z_pin, (forces[3] > force_threshold))
-    GPIO.output(_negative_z_pin, (forces[3] < (force_threshold * -1)))
+    GPIO.output(_positive_x_pin, (forces[1] > force_threshold_x))
+    GPIO.output(_negative_x_pin, (forces[1] < (force_threshold_x * -1)))
+    GPIO.output(_positive_y_pin, (forces[2] > force_threshold_y))
+    GPIO.output(_negative_y_pin, (forces[2] < (force_threshold_y * -1)))
+    GPIO.output(_positive_z_pin, (forces[3] > force_threshold_z))
+    GPIO.output(_negative_z_pin, (forces[3] < (force_threshold_z * -1)))
 
     positive_leds = [_positive_x_pin, _positive_y_pin, _positive_z_pin]
     negative_leds = [_negative_x_pin, _negative_y_pin, _negative_z_pin]
 
-    for i in range len(forces):
-        if forces[i] > force_threshold:
+    #for i in range (len(forces)):
+        #if forces[i] > force_threshold:
             #color = red
             #LED = positive[i]
-        if forces[i] < (force_threshold * -1):
+        #if forces[i] < (force_threshold * -1):
             #color = red
             #LED = negative[i]
-        if forces[i] < force_threshold && forces[i] > touch_threshold:
+        #if forces[i] < force_threshold && forces[i] > touch_threshold:
             #color = green
             #LED = positive[i]
-        if forces[i] > (force_threshold * -1) && forces[i] < (touch_threshold * -1):
+        #if forces[i] > (force_threshold * -1) && forces[i] < (touch_threshold * -1):
             #color = green
             #LED = negative[i]
-        else:
+        #else:
             #positive and negative of [i] are off
 
     # Toggle with
@@ -198,20 +199,20 @@ def indicate_status(forces):
 def run_script(program_timeout):
     """
     Runs the loop of get force-->log force-->indicate status for a defined time period
-    :param XXX: 
+    :param program_timeout: From parsed args, default 20 min
     :return: whatever the function returns
     """
-    print "Logging force data, press ^C to quit"
+    print ("Logging force data, press ^C to quit")
 
     # Define interval, in seconds, for force readings
-    interval = 0.01
+    interval = 0.25
 
     # log the start time of the loop for calculating timeout
     start_time = datetime.datetime.now()
     current_time = start_time
     last_reading = start_time
 
-    while (current_time - start_time).total_seconds() < (program_timeout * 60):
+    while (current_time - start_time).total_seconds() < float(program_timeout * 60):
         
         current_time = datetime.datetime.now()
         
@@ -225,22 +226,22 @@ def run_script(program_timeout):
                 log_force(forces)
                 indicate_status(forces)
 
-                last_reading = curent_time
+                last_reading = current_time
                 
                 GPIO.output(_activity_pin, False) 
     
         except KeyboardInterrupt:
             # Quit cleanly if the user presses ^C
-            print "User interrupt...quitting..."
+            print ("User interrupt...quitting...")
             GPIO.output(_activity_pin, False)
             GPIO.cleanup()
             s.close()
-            file.close()
+            #file.close()
             return False
             
     
     # Quit cleanly if the program times out
-    print "Program timed out...quitting..."
+    print ("Program timed out...quitting...")
     GPIO.output(_activity_pin, False)
     GPIO.cleanup()
     return True
@@ -253,7 +254,7 @@ if __name__ == "__main__":
     :param XXX: some useful info on what it is or where it's from
     :return: whatever the function returns
     """
-    print "Welcome to the waypoint trainer"
+    print ("Welcome to the waypoint trainer")
     # make the CLI
     parser = argparse.ArgumentParser(description="Script for running automated force logging.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -279,8 +280,10 @@ if __name__ == "__main__":
         port_array = []
         for i in range(256):
             try:
+                #print(str(i))
                 s = serial.Serial('/dev/ttyACM' + str(i))
-                port.append((s.portstr))
+                port_array.append((s.portstr))
+                s.close()
             except:
                 pass
 
@@ -291,27 +294,28 @@ if __name__ == "__main__":
         # This is fragile and should be edited to check if the device matches ID: 16d0:0c21
         else:
             port = port_array[0]
+            #print("Port: " + str(port))
 
         # set up the serial port 
-        s = Serial(port=port, baudrate=115200, timeout=0.1, bytesize=EIGHTBITS,
-                   stopbits=STOPBITS_ONE, parity=PARITY_NONE)
-
+        s = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=0.1)
+        print("Serial set...")
         # run init functions
         sensor_init(parsed_args.data_format)
         logs_init()
-        # indicator_init()
-
+        indicator_init()
+        GPIO.output(_power_pin, True)
         # Do the things!
         run_script(program_timeout=parsed_args.program_timeout)
     except Exception:
-        print("Error Initializing CLI")
+        print("Error")
+        
+    
         
 
 
     ####################################################
     #                       TODOs                      #
-    # - change all vars from camelCase to under_scores #
-    # - clean up pseudocode
+    # - Quit better when in the get_force() fcn
     # - 
     # - check usb device ID and vendor, compare to sensor
     # - 
